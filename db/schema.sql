986c884efa681935e73e4e5cc18e85d917aa8bff
Afnan Shaikh <afnanahmed.shaikh@gmil.com>
2020-08-18 17:36:29 -0400

    sql database changes

--- db/schema.sql
+++ db/schema.sql
@@ -8,14 +8,12 @@ CREATE TABLE department (
     PRIMARY KEY(id)
 );
 
-CREATE TABLE role (
-    id INT AUTO_INCREMENT,
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL, 
     salary DECIMAL(6,2) NOT NULL, 
     department_id INT NOT NULL, 
-    PRIMARY KEY(id),
-    FOREIGN KEY(department_id)
-    REFERENCES department(id)
+    FOREIGN KEY(department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
@@ -26,10 +24,9 @@ CREATE TABLE employee (
     manager_id INT,
     PRIMARY KEY(id),
 
-    FOREIGN KEY(role_id)
-    REFERENCES role(id),
+    FOREIGN KEY(roles_id)
+    REFERENCES roles(id),
 
-    FOREIGN KEY(manager_id)
-    REFERENCES employee(id)
+    FOREIGN KEY(manager_id) REFERENCES employee(id)
 
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -11,8 +11,8 @@ INSERT INTO role (title, salary, department_id) VALUES ("Social Media Manager", 
 INSERT INTO role (title, salary, department_id) VALUES ("Director", 100, 1);
 INSERT INTO role (title, salary, department_id) VALUES ("Director", 100, 4);
 
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("David", "Blake", 2);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Alex", "Whiteman", 1);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Kate", "Peter", 3);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Rachael", "Sandoe", 4);
-INSERT INTO employee (first_name, last_name, role_id) VALUES ("Allison", "Halep", 5);
+INSERT INTO employee (first_name, last_name, roles_id) VALUES ("David", "Blake", 2);
+INSERT INTO employee (first_name, last_name, roles_id) VALUES ("Alex", "Whiteman", 1);
+INSERT INTO employee (first_name, last_name, roles_id) VALUES ("Kate", "Peter", 3);
+INSERT INTO employee (first_name, last_name, roles_id) VALUES ("Rachael", "Sandoe", 4);
+INSERT INTO employee (first_name, last_name, roles_id) VALUES ("Allison", "Halep", 5);
