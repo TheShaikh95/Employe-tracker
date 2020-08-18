3645788a7a04e0a4aa788f9a5fcff582bfe9c6a2
Afnan Shaikh <afnanahmed.shaikh@gmil.com>
2020-08-18 17:46:16 -0400

    spelling errors in seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,11 +5,11 @@ INSERT INTO department (name) VALUES ("Marketing");
 INSERT INTO department (name) VALUES ("Information Technology");
 INSERT INTO department (name) VALUES ("Corporate");
 
-INSERT INTO role (title, salary, department_id) VALUES ("Analyst", 70, 3);
-INSERT INTO role (title, salary, department_id) VALUES ("Communications Associate", 50, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Social Media Manager", 50, 2);
-INSERT INTO role (title, salary, department_id) VALUES ("Director", 100, 1);
-INSERT INTO role (title, salary, department_id) VALUES ("Director", 100, 4);
+INSERT INTO roles (title, salary, department_id) VALUES ("Analyst", 70, 3);
+INSERT INTO roles (title, salary, department_id) VALUES ("Communications Associate", 50, 2);
+INSERT INTO roles (title, salary, department_id) VALUES ("Social Media Manager", 50, 2);
+INSERT INTO roles (title, salary, department_id) VALUES ("Director", 100, 1);
+INSERT INTO roles (title, salary, department_id) VALUES ("Director", 100, 4);
 
 INSERT INTO employee (first_name, last_name, roles_id) VALUES ("David", "Blake", 2);
 INSERT INTO employee (first_name, last_name, roles_id) VALUES ("Alex", "Whiteman", 1);
